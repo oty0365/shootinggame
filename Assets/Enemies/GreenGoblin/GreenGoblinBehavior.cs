ff2b9e795a8ed5df0e8c482c4d9833b0840fdc64
oty0365 <163986958+oty0365@users.noreply.github.com>
2024-06-25 21:57:51 +0900

    1
    1

--- Assets/Enemies/GreenGoblin/GreenGoblinBehavior.cs
+++ Assets/Enemies/GreenGoblin/GreenGoblinBehavior.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GreenGoblinBehavior : MonoBehaviour
@@ -7,11 +8,17 @@ public class GreenGoblinBehavior : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 startpos;
     public float movespeed;
+    private SpriteRenderer sr;
+    [SerializeField] private Sprite[] sprites;  
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
         startpos.y = 14;
+        startpos.x = gameObject.transform.position.x;
+        rb.position = startpos;
+        sr.sprite = sprites[0];
     }
 
     // Update is called once per frame
@@ -21,6 +28,14 @@ public class GreenGoblinBehavior : MonoBehaviour
         if (gameObject.transform.position.y <= -5)
         {
             rb.position = new Vector3(gameObject.transform.position.x,7,0);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D _collider2D)
+    {
+        if (_collider2D.gameObject.CompareTag("bullet"))
+        {
+            Debug.Log("!");
         }
     }
 }
