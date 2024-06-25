f1af3a36b40dbfb0acd7444b1c1e1c300f179db4
oty0365 <163986958+oty0365@users.noreply.github.com>
2024-06-25 23:32:11 +0900

    스테이지 시스템과 적 피격 구현

--- Assets/Enemies/GreenGoblin/GreenGoblinBehavior.cs
+++ Assets/Enemies/GreenGoblin/GreenGoblinBehavior.cs
@@ -9,7 +9,9 @@ public class GreenGoblinBehavior : MonoBehaviour
     private Vector2 startpos;
     public float movespeed;
     private SpriteRenderer sr;
-    [SerializeField] private Sprite[] sprites;  
+    [SerializeField] private Sprite[] sprites;
+    public int hp;
+    private bool isdistroid;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,8 @@ public class GreenGoblinBehavior : MonoBehaviour
         startpos.x = gameObject.transform.position.x;
         rb.position = startpos;
         sr.sprite = sprites[0];
+        rb.gravityScale = 0;
+        isdistroid = false;
     }
 
     // Update is called once per frame
@@ -27,15 +31,41 @@ public class GreenGoblinBehavior : MonoBehaviour
         rb.velocity = Vector2.down * movespeed;
         if (gameObject.transform.position.y <= -5)
         {
+            if (hp <= 0)
+            {
+                StageManager.kills++;
+                Destroy(gameObject);
+            }
             rb.position = new Vector3(gameObject.transform.position.x,7,0);
         }
+        
     }
 
     void OnTriggerEnter2D(Collider2D _collider2D)
     {
         if (_collider2D.gameObject.CompareTag("bullet"))
         {
-            Debug.Log("!");
+            hp--;
+        }
+
+        switch (hp)
+        {
+            case 2:
+                sr.sprite = sprites[1];
+                break;
+            case 1:
+                sr.sprite = sprites[2];
+                break;
+            case 0:
+                isdistroid = true;
+                sr.sprite = sprites[3];
+                rb.gravityScale = 14f;
+                break;
+            default:
+                isdistroid = true;
+                sr.sprite = sprites[3];
+                rb.gravityScale = 14f;
+                break;
         }
     }
 }
--- Assets/Stages/StageManager.cs
+++ Assets/Stages/StageManager.cs
@@ -1,20 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
+    public static int stage;
+    public static int kills;
     [SerializeField] private Animator ani;
+    [SerializeField] private TextMeshProUGUI Text;
+    [SerializeField] private int[] stagekillrequiered;
     // Start is called before the first frame update
     void Start()
     {
+        stage = 1;
         StageStart();
+    }
+
+    private void Update()
+    {
+        if (kills == stagekillrequiered[stage - 1])
+        {
+            Text.text = "스테이지 클리어";
+            ani.SetTrigger("NewStage");
+            kills = 0;
+        }
     }
 
     // Update is called once per frame
 
     public void StageStart()
     {
+        Text.text = "스테이지 " + stage;
         ani.SetTrigger("NewStage");
     }
 }
