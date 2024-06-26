09c6ae272518dd4c6290d535df5890bb3490af4e
oty0365 <163986958+oty0365@users.noreply.github.com>
2024-06-26 23:30:48 +0900

    save try

---
+++ Assets/Player/Script/PlayerStatus.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Windows;
+using File = UnityEngine.Windows.File;
+
+public class PlayerStatus : MonoBehaviour
+{
+
+    public static int playerhp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/System/DataManager/Datamanager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Datamanager : MonoBehaviour
+{
+    public string path;
+    public string file = "player.txt";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Load()
+    {
+        
+    }
+    public void Save()
+    {
+        string json = JsonUtility.ToJson(PlayerStatus.playerhp);
+        WriteToFile(file, json);
+    }
+
+    private void WriteToFile(string fileName, string json)
+    {
+        string path = GetFilePath(fileName);
+        FileStream fileStream = new FileStream(path, FileMode.Create);
+        using (StreamWriter writer = new StreamWriter(fileStream))
+        {
+            writer.Write(json);
+        }
+    }
+
+    private string GetFilePath(string fileName)
+    {
+        return Application.persistentDataPath + "/" + fileName;
+    }
+
+    private string ReadFromFile(string fileName)
+    {
+        string path = GetFilePath(fileName);
+        if (File.Exists(path))
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string json = reader.ReadToEnd();
+                return json;
+            }
+        }
+    }
+}
